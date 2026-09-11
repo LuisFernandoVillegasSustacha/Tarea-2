71de385e933f3416c9e786b4dfe69afaf4b0253e
LuisFernandoVillegasSustacha <ferluisvi18@gmail.com>
2026-09-11 18:40:46 -0400

    Tarea2

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
-let nombre = document.getElementById("nombre");
-let juego = document.getElementById("nombre del juego");
-let aspectos = document.getElementById("recomendacion del juego");
-let recomendaria = document.getElementById("recomendaria");
+
+let pregunta1 = document.getElementById("pregunta1");
+let pregunta2 = document.getElementById("pregunta2");
+let pregunta3 = document.getElementById("pregunta3");
+let pregunta4 = document.getElementById("pregunta4");
+let pregunta5 = document.getElementById("pregunta5");
+
 let resultado = document.getElementById("respuesta");
 let btn = document.getElementById("boton");
+
 btn.addEventListener("click", (event) => {
     event.preventDefault();
-    resultado.innerHTML = "Nombre: " + nombre.value + "<br>" + " Juego: " + juego.value + "<br>" + "Aspectos: " + aspectos.value + "<br>" + "Recomendaría: " + recomendaria.value;
+
+    resultado.innerHTML = 
+        "Género de videojuego: " + pregunta1.value + "<br>" +
+        "Frecuencia de juego: " + pregunta2.value + "<br>" +
+        "Género musical: " + pregunta3.value + "<br>" +
+        "Cuándo escucha música: " + pregunta4.value + "<br>" +
+        "Actividad favorita: " + pregunta5.value;
+
     console.log(resultado.innerHTML);
 });
+
+
