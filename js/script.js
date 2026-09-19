3a9a68ce8e2a99da77dfc882b70b7bffdca8bdb7
Luis_Fernando_Villegas_Sustacha <ferluisvi18@gmail.com>
2026-09-18 21:18:10 -0400

    Implement scoring system for user responses
    Added scoring logic based on user input from questions.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-
+```javascript
 let pregunta1 = document.getElementById("pregunta1");
 let pregunta2 = document.getElementById("pregunta2");
 let pregunta3 = document.getElementById("pregunta3");
@@ -11,14 +11,33 @@ let btn = document.getElementById("boton");
 btn.addEventListener("click", (event) => {
     event.preventDefault();
 
-    resultado.innerHTML = 
-        "Género de videojuego: " + pregunta1.value + "<br>" +
-        "Frecuencia de juego: " + pregunta2.value + "<br>" +
-        "Género musical: " + pregunta3.value + "<br>" +
-        "Cuándo escucha música: " + pregunta4.value + "<br>" +
-        "Actividad favorita: " + pregunta5.value;
+    let puntaje = 0;
+
+    if (pregunta1.value == "Acción") {
+        puntaje = puntaje + 20;
+    }
+
+    if (pregunta2.value == "Todos los días") {
+        puntaje = puntaje + 20;
+    }
+
+    if (pregunta3.value == "Rock") {
+        puntaje = puntaje + 20;
+    }
+
+    if (pregunta4.value == "En mi tiempo libre") {
+        puntaje = puntaje + 20;
+    }
+
+    if (pregunta5.value == "Escuchar música") {
+        puntaje = puntaje + 20;
+    }
+
+    resultado.innerHTML =
+        "Tu puntaje es: " + puntaje + " / 100";
 
     console.log(resultado.innerHTML);
 });
+```
 
 
